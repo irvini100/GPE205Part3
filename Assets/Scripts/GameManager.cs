010e477e316ddf80d566fcfa8c8dd49ffdab9483
Irvin Ingram <82079816+irvini100@users.noreply.github.com>
2021-09-27 01:55:11 -0400

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    //Setup our singleton.
+    public static GameManager instance;
+    public List<TankData> tanks;
+    public List<InputController> players;
+
+    void Awake()
+    {
+        //If singleton already exists.
+        if (instance != null)
+        {
+            //Self-destruct
+            Destroy(gameObject);
+        }
+        else
+        {
+            //Otherwise, I am the instance.
+            instance = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
